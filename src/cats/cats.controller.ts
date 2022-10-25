89d9765503f674e943e566c53fe32c5d75fe5a4c
hg-ian-choi <hg.ian.choi@gmail.com>
2022-10-25 09:46:14 +0900

    try get request

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,6 +1,23 @@
 /* cats/cats.controller.ts */
 
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  /**
+   * GET请求：
+   *   默认响应状态码： 200
+   */
+  @Get()
+  @HttpCode(205) // 可更改响应状态码
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+
+  // using express Request
+  @Get()
+  findAll2(@Req() request: Request): string {
+    return 'This action returns all cats';
+  }
+}
