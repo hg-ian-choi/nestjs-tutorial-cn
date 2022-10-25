a9a8816f3f6f6999cd7010fe34f5598e9003a68e
hg-ian-choi <hg.ian.choi@gmail.com>
2022-10-25 09:46:52 +0900

    try post request

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,20 @@
 /* cats/cats.controller.ts */
 
-import { Controller, Get, HttpCode, Req } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
+  /**
+   * POST请求：
+   *   默认响应状态码： 201
+   */
+  @Post()
+  @HttpCode(204) // 可更改响应状态码
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
   /**
    * GET请求：
    *   默认响应状态码： 200
